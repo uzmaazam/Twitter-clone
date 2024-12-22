15006ec2412d22e8a96069691ef25623a17ee64a
uzmaazam <uzmaazam333@gmail.com>
2024-12-22 04:29:45 -0500

    Update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
-import notificationRoutes from "./routes/notification.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 import connectMongoDB from "./db/connectMongoDb.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
